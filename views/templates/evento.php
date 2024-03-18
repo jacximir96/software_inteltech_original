7b1eb2cdde8b3d9d4374075c5211118ea3edd2c9
AdrianPalacios02 <apalacios.loicksolution@gmail.com>
2024-03-18 14:59:59 -0500

    normalidad en la parte de servicios

--- views/templates/evento.php
+++ views/templates/evento.php
@@ -2,16 +2,16 @@
     <!-- <p class="evento__hora"><?php echo $evento->hora->hora; ?></p> -->
     <div class="evento__informacion">
         <h4 class="evento__nombre">ELABORACIÓN DE EXPEDIENTES TÉCNICOS</h4>
-        <p class="evento__introduccion"></p>
+        <p class="evento__introduccion">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis veniam, aliquam similique, omnis est optio impedit</p>
         <div class="evento__autor-info">
-            <!-- <picture>
+            <picture>
                 <source srcset="img/speakers/<?php echo $evento->ponente->imagen; ?>.webp" type="image/webp">
                 <source srcset="img/speakers/<?php echo $evento->ponente->imagen; ?>.png" type="image/png">
                 <img class="evento__imagen-autor" loading="lazy" width="200" height="300" src="img/speakers/<?php echo $evento->ponente->imagen; ?>.png" alt="Imagen Ponente">
-            </picture> -->
-            <!-- <p class="evento__autor-nombre">
+            </picture>
+            <p class="evento__autor-nombre">
                 <?php echo $evento->ponente->nombre . " " . $evento->ponente->apellido; ?>
-            </p> -->
+            </p>
         </div>
     </div>
 </div>
